5ebd8da7022bbe4962a226e63be95dc5eaf753ab
Gregory Maslov <gmaslov@bootis.org>
2012-10-11 16:15:28 -0400

    Add visual "excerpt" markers.

--- public/function.js
+++ public/function.js
@@ -3,17 +3,21 @@
 // beep beep, encapsulation violation
 var interview_id = 'interviews:'+document.URL.split('/').pop();
 // _DATASET_ID is a global set by some JS emitted elsewhere on the page.
+var segments = {}; // set of sentence ids that have been marked
 
 function insertSegment(span,dontPost) {
   var sentence = $(span);
   var sentence_id = sentence.attr('id');
   var hr = '<hr class="segment" sentence_id="'+sentence_id+'"/>';
+  segments[sentence_id] = true;
   // When this is the first sentence in a speechblock, put the <hr> before the speaker's name so it looks nicer.
   if(sentence.prev().hasClass('speaker')) {
     sentence.prev().before(hr);
   } else {
     sentence.before(hr);
   }
+  updateMarkers();
+
   if(!dontPost) {
     $.post('/insertSegment', { dataset_id: _DATASET_ID, interview_id: interview_id, sentence_id: sentence_id });
   }
@@ -21,8 +25,37 @@ function insertSegment(span,dontPost) {
 
 function deleteSegment(span) {
   var sentence_id = $(span).attr('sentence_id');
+  delete segments[sentence_id];
   $(span).remove();
   $.post('/deleteSegment', { dataset_id: _DATASET_ID, interview_id: interview_id, sentence_id: sentence_id });
+  updateMarkers();
+}
+
+function updateMarkers() {
+  var inside = false;
+  var marker = null;
+  var prev_bottom = 0;
+  var marker_top = 0;
+  $('.marker').remove();
+  $('span.sentence').each(function() {
+    if($(this).attr('id') in segments) {
+      inside = !inside;
+
+      if(inside) {
+        // start a new marker
+        var o = $(this).offset();
+        marker = $('<div class="marker"/>');
+        marker.css('top', o.top);
+        marker.css('left', o.left - 15);
+        marker_top = o.top;
+      } else {
+        // end the previous marker
+        marker.css('height', prev_bottom - marker_top);
+        marker.appendTo(document.body);
+      }
+    }
+    prev_bottom = $(this).offset().top + $(this).offset().height;
+  });
 }
 
 Zepto(function($) {
@@ -30,5 +63,6 @@ Zepto(function($) {
   $('body')
     .on('click', 'span.sentence', function(e){insertSegment(e.target)})
     .on('click', 'hr', function(e){deleteSegment(e.target)})
+  updateMarkers();
 });
 
