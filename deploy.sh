06d6bc77bd80ede740d4f2216eaf98e6f81b77e5
Ryan Shaw <ryanshaw@unc.edu>
2012-11-01 22:13:09 -0400

    Add deployment script.

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+die () {
+  echo >&2 "$@"
+  exit 1
+}
+
+[ "$#" -eq 1 ] || die "Usage: deploy.sh hostname"
+
+HOST=$1
+
+git archive --format=zip --prefix=segment/ HEAD -o segment.zip
+scp segment.zip $HOST:
+ssh $HOST 'unzip -u segment.zip && cd segment && npm install'
+rm -f segment.zip
+
